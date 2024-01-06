c2e20e9c070760af50c05f91f9d60dd44830ad24
DECHENAUD RAPHAEL p2106803 <raphael.dechenaud@etu.univ-lyon1.fr>
2024-01-06 21:32:17 +0100

    change localhost to 127.0.0.1

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/DBFrogDomo_API', { useNewUrlParser: true })
+mongoose.connect('mongodb://127.0.0.1:27017/DBFrogDomo_API', 
+{  
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
