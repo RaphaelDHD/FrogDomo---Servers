d8701d62188764921b2d9d6de1d32840d209c825
Rakel TAFA <rakeltafa.pro@gmail.com>
2024-01-16 18:31:05 +0100

    Ajout du champ sonnerie pour l'alarme "rung"

--- models/user.js
+++ models/user.js
@@ -57,6 +57,10 @@ const userSchema = new mongoose.Schema({
             type: Boolean,
             default: false
         },
+        rung: {
+            type: Boolean,
+            default: false,
+        },
         date: {
             type: Date
         } 
